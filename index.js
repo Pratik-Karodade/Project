36104bc8dfd5524fd10af191609279bf89ff7d1b
Pratik-Karodade <karodadepratik@gmail.com>
2021-08-03 10:39:23 +0530

    signup created
    signuop created

---
+++ index.js
@@ -0,0 +1,6 @@
+	var userMail = document.getElementById("email_field").value;
+    var userPass = document.getElementById("pass_field").value;
+function login(){
+
+    window.alert("User name: "+userMail+"\nPassword :" +userPass);
+} 
